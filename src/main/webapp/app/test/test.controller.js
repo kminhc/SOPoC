d7626f6057ba2bcfd19b4823c76070e9754ef54e
Kong Minh Cap <kminhc@gmail.com>
2016-04-25 17:02:42 +0200

    event update on calendar

--- src/main/webapp/app/test/test.controller.js
+++ src/main/webapp/app/test/test.controller.js
@@ -5,12 +5,12 @@
         .module('soPoCApp')
         .controller('TestController', TestController);
 
-    TestController.$inject = ['$scope', 'Principal', 'LoginService', 'Event', 'AlertService', 'Eventsource'];
+    TestController.$inject = ['$scope', 'Principal', 'LoginService', 'Event', 'AlertService', 'Eventsource', 'uiCalendarConfig'];
 
-    function TestController ($scope, Principal, LoginService, Event, AlertService, Eventsource) {
+    function TestController ($scope, Principal, LoginService, Event, AlertService, Eventsource, uiCalendarConfig) {
         var vm = this;
         /*Get all Events*/
-        $scope.eventList = Event.query();
+        $scope.events = Event.query();
         /*Get all EventSources for dropdown list*/
         vm.eventSourcesList = Eventsource.query();
 
@@ -50,6 +50,13 @@
             } else {
                 Event.save(vm.event, onSaveSuccess, onSaveError);
             }
+            $scope.events = Event.query();
+            $scope.events.$promise.then(function(result){
+
+                $scope.events = result;
+                uiCalendarConfig.calendars.myCalendar.fullCalendar('removeEvents');
+                uiCalendarConfig.calendars.myCalendar.fullCalendar('addEventSource', $scope.events);
+            })
         };
 
         /*Setting is Saving state*/
@@ -69,26 +76,26 @@
             vm.datePickerOpenStatus[date] = true;
         };
 
-       /* Set reference to eventsources for the calendar
-        as referred here: "http://fullcalendar.io/docs/event_data/Event_Source_Object/"*/
-        $scope.eventSources =[{
-            events: $scope.eventList,
-            color: 'red',   // an option!
-            textColor: 'black' // an option!
-        }];
-
         /*Configuration for the calendar*/
         $scope.uiConfig = {
             calendar:{
                 height: 450,
                 editable: true,
                 header:{
-                  left: 'month basicWeek basicDay agendaWeek agendaDay',
-                  center: 'title',
-                  right: 'today prev,next'
+                    left: 'month basicWeek basicDay agendaWeek agendaDay',
+                    center: 'title',
+                    right: 'today prev,next'
                 }
             }
         };
+
+       /* Set reference to eventsources for the calendar
+        as referred here: "http://fullcalendar.io/docs/event_data/Event_Source_Object/"*/
+        $scope.eventSources =[{
+            events: $scope.events
+           /* color: 'red',   // an option!
+            textColor: 'black' // an option!*/
+        }];
 
     }
 })();
